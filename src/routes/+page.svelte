66e3f5478398311dc9d0e7e891f4f6b1887a4166
ZERO <103266275+TSC-Home@users.noreply.github.com>
2024-06-12 11:14:49 +0200

    add a new editor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,90 @@
 <script lang="ts">
+	import type monaco from 'monaco-editor';
+	import { onMount } from 'svelte';
 	import { writable, derived } from 'svelte/store';
+	import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+	import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+	import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+	import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+	import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+	import { tailwindClasses } from '$lib/index';
 
 	// Stores to manage editor content and placeholders
 	const editorContent = writable('');
 	const placeholders = writable<{ [key: string]: string }>({});
 
-	// Function to update the preview based on editor content
-	const updatePreview = (html: string) => {
+	let divEl: HTMLDivElement;
+	let editor: monaco.editor.IStandaloneCodeEditor;
+	let Monaco: any;
+
+	const initialEditorValue = `
+    <div class="flex text-3xl h-[400px] bg-zinc-900 p-12">
+        <div class="w-[800px] p-8 flex flex-col justify-center items-center text-center border-8 rounded-[50px] border-rose-600 text-white background">
+            <p class="font-bold" style="font-size: 80px">{title}</p>
+            <p class="mt-4">{description}</p>
+        </div>
+    </div>`;
+
+	async function initializeEditor() {
+		// @ts-ignore
+		self.MonacoEnvironment = {
+			getWorker: function (_moduleId: any, label: string) {
+				if (label === 'json') {
+					return new jsonWorker();
+				}
+				if (label === 'css' || label === 'scss' || label === 'less') {
+					return new cssWorker();
+				}
+				if (label === 'html' || label === 'handlebars' || label === 'razor') {
+					return new htmlWorker();
+				}
+				if (label === 'typescript' || label === 'javascript') {
+					return new tsWorker();
+				}
+				return new editorWorker();
+			}
+		};
+
+		Monaco = await import('monaco-editor');
+		editor = Monaco.editor.create(divEl, {
+			value: initialEditorValue,
+			language: 'html',
+			minimap: {
+				enabled: false
+			}
+		});
+
+		Monaco.languages.registerCompletionItemProvider('html', {
+			provideCompletionItems: () => {
+				const suggestions = tailwindClasses.map((className) => ({
+					label: className,
+					kind: Monaco.languages.CompletionItemKind.Keyword,
+					insertText: className
+				}));
+				return { suggestions: suggestions };
+			}
+		});
+
+		editor.onDidChangeModelContent(() => {
+			const value = editor.getValue();
+			updatePreview(value);
+		});
+
+		// Initialize preview with the initial value
+		updatePreview(initialEditorValue);
+	}
+
+	onMount(() => {
+		initializeEditor();
+
+		return () => {
+			if (editor) {
+				editor.dispose();
+			}
+		};
+	});
+
+	function updatePreview(html: string) {
 		editorContent.set(html);
 		const matches = html.match(/{(.*?)}/g) || [];
 		const newPlaceholders: { [key: string]: string } = {};
@@ -17,37 +95,28 @@
 			}
 		});
 		placeholders.set(newPlaceholders);
-	};
+	}
 
-	// Function to handle input changes for placeholders
-	const handleInput = (name: string, value: string) => {
+	function handleInput(name: string, value: string) {
 		placeholders.update((current) => {
 			current[name] = value;
 			return { ...current };
 		});
-	};
+	}
 
-	const handleTextareaInput = (event: Event) => {
-		const target = event.target as HTMLTextAreaElement;
-		updatePreview(target.value);
-	};
-
-	const handlePlaceholderInput = (event: Event, name: string) => {
+	function handlePlaceholderInput(event: Event, name: string) {
 		const target = event.target as HTMLInputElement;
 		handleInput(name, target.value);
-	};
-
-	// Embed Tailwind CSS into the preview HTML
-	const tailwindCSS = `
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-  `;
+	}
 
 	const previewHTML = derived([editorContent, placeholders], ([$editorContent, $placeholders]) => {
 		let html = $editorContent;
 		for (const [key, value] of Object.entries($placeholders)) {
 			html = html.replace(new RegExp(`{${key}}`, 'g'), value);
 		}
-		return `${tailwindCSS}<div class="p-4">${html}</div>`;
+		return `
+        <script src="https://cdn.tailwindcss.com"><\/script>
+        <div class="h-full w-full max-w-[800px] max-h-[400px]">${html}</div>`;
 	});
 </script>
 
@@ -56,13 +125,26 @@
 		<h1 class="text-3xl font-bold">Svelte Dynamic OG Image Generator</h1>
 	</div>
 	<div class="flex h-[85%] space-x-4 p-4">
-		<div class="flex h-full w-1/2 flex-col rounded-md border-2 border-gray-200 p-4">
-			<textarea
-				class="mb-4 h-full w-full border-2 border-gray-200 p-2"
-				placeholder="Schreibe HTML mit"
-				on:input={handleTextareaInput}
-			></textarea>
-			<div>
+		<div class="flex h-full w-1/2 flex-col rounded-md border-2 border-gray-200">
+			<div bind:this={divEl} class="h-full w-full" />
+		</div>
+		<div class="h-full w-1/2 p-4">
+			<iframe
+				class=" mx-auto h-full max-h-[400px] w-full max-w-[800px] border bg-white shadow-lg"
+				srcdoc={$previewHTML}
+				title="preview"
+			></iframe>
+			<div class="  flex space-x-2">
+				<button
+					class=" p-2 text-sm font-semibold uppercase text-gray-500 hover:text-gray-800 hover:underline"
+					>Save</button
+				>
+				<button
+					class=" p-2 text-sm font-semibold uppercase text-gray-500 hover:text-gray-800 hover:underline"
+					>Get Url</button
+				>
+			</div>
+			<div class="flex flex-col space-y-2">
 				{#each Object.keys($placeholders) as name}
 					<input
 						type="text"
@@ -74,29 +156,15 @@
 				{/each}
 			</div>
 		</div>
-		<div class="h-full w-1/2 p-4">
-			<div
-				class="relative mx-auto h-full max-h-[630px] w-full max-w-[1200px] overflow-hidden border bg-white shadow-lg"
-			>
-				<iframe class="absolute inset-0 h-full w-full" srcdoc={$previewHTML}></iframe>
-			</div>
-		</div>
 	</div>
-	<footer
-		class="flex items-center justify-center space-x-2 p-4 text-sm font-semibold uppercase text-gray-500"
-	>
-		<a class="hover:text-gray-800 hover:underline" href="https://synthetix.me">Synthetix.me🦕</a>
-		<a class="hover:text-gray-800 hover:underline" href="https://github.com/tsc-home">My Github🔥</a
-		>
-		<a
-			class="hover:text-gray-800 hover:underline"
-			href="https://github.com/tsc-home/svelte-dynamic-og-img-generator">Source Code❤️</a
-		>
-	</footer>
 </div>
-
-<style>
-	textarea {
-		resize: none;
-	}
-</style>
+<footer
+	class=" bottom-0 mt-8 flex items-center justify-center space-x-2 p-4 text-sm font-semibold uppercase text-gray-500"
+>
+	<a class="hover:text-gray-800 hover:underline" href="https://synthetix.me">Synthetix.me🦕</a>
+	<a class="hover:text-gray-800 hover:underline" href="https://github.com/tsc-home">My Github🔥</a>
+	<a
+		class="hover:text-gray-800 hover:underline"
+		href="https://github.com/tsc-home/svelte-dynamic-og-img-generator">Source Code❤️</a
+	>
+</footer>
